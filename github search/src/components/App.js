420e8cf882e086b136bed6bebc0516ce5e959848
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-05-08 17:52:10 +0200

    card

--- github search/src/components/App.js
+++ github search/src/components/App.js
@@ -1,6 +1,6 @@
 import '../styles/App.css';
 import React, { useState, useEffect } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Card, Image, Icon } from 'semantic-ui-react';
 
 
 function App() {
@@ -13,22 +13,73 @@ function App() {
   const [userInput, setUserInput] = useState('');
   const [error, setError] = useState('');
 
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
 
+  const setData = ({ name, login, followers, following, public_repos, avatar_url }) => {
+    setName(name);
+    setUserName(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+  };
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value)
+  }
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+    then(res => res.json())
+    then(data => {
+      setData(data)
+    })
+  }
   return (
     <div className="App">
       <div className="navbar">
         Github Search
       </div>
       <div className="search">
-        <Form onSubmit={this.handleSubmit}>
+        <Form>
           <Form.Group>
             <Form.Input
-              placeholder='Name'
-              name='name'
-            />
-            <Form.Button content='Submit' />
+              placeholder='Name' name='name' />
+            <Form.Button content='Search' />
           </Form.Group>
         </Form>
+      </div>
+      <div className="card">
+        <Card>
+          <Image src={avatar} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Header>{userName}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              {followers} Followers
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              {following} Following
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              {repos} Repositories
+            </a>
+          </Card.Content>
+        </Card>
       </div>
     </div>
   );
--- github search/src/index.js
+++ github search/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './src/styles/app.css';
+import './styles/App.css';
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
