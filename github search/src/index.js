3082982038b191788b5dcf9686549c1964875953
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-05-08 17:19:07 +0200

    semantic library

--- github search/src/index.js
+++ github search/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './index.css';
+import './src/styles/app.css';
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
