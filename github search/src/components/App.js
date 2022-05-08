c0109509c1c870da3c76abecfb193b6fdd5023e0
Mariamafalda2021 <mariarezgarcia@gmail.com>
2022-05-08 16:43:39 +0200

    semantic library

--- github search/src/components/App.js
+++ github search/src/components/App.js
@@ -1,9 +1,35 @@
 import '../styles/App.css';
+import React, { useState, useEffect } from 'react';
+import { assertVariableDeclarator } from '@babel/types';
+
 
 function App() {
+  const [name, setname] = useState('');
+  const [userName, setUserName] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState('');
+
+
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <div className="navbar">
+        Github Search
+      </div>
+      <div className="search">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder='Name'
+              name='name'
+            />
+            <Form.Button content='Submit' />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
